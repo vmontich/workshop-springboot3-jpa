8a45127fa9831da272e8e9d414214f1d7aa10a6a
Vitor Montich <vmontich@hotmail.com>
2023-01-12 21:53:07 -0300

    Exception handling - update

--- src/main/java/com/vitormontich/course/services/UserService.java
+++ src/main/java/com/vitormontich/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.vitormontich.course.repositories.UserRepository;
 import com.vitormontich.course.services.exceptions.DatabaseException;
 import com.vitormontich.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+			
 	}
 	
 	private void updateData(User entity, User obj) {
